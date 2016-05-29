243f9f1980bd99f4e5ea0e47a7bdc0195771972e
Muhammad Irfan <Muhammad Irfan>
2016-05-29 20:32:31 +0700

    added book/add api

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 var bodyParser = require('body-parser');
 var thirdPartyRequest = require('request');
 
+var mongodb = require("mongodb");
+var mongodbclient = mongodb.MongoClient;
+var db_url = "mongodb://"+ process.env.MONGODB_USER +":"+ process.env.MONGODB_PASSWORD +"@ds013951.mlab.com:13951/fcc-challenge";
+
 app.use(bodyParser());
 
 app.get('/', function(req,res){
@@ -30,11 +34,37 @@ app.get('/book/search/:query', function(req,res){
 		process.env.API_URL + "&q=" + req.params.query,
 		{},
 		function (err, response, body) {
-			console.log(body);
+			res.status(200).json(JSON.parse(body));
 			res.end();
 		}
 	);
 });
+
+app.post('/book/add', function(req,res){
+	var data = JSON.parse(req.body.data);
+	if (!data.title || !data.description || !data.imageLinks) {
+		throw Error('wrong data format');
+		return;
+	}
+
+	mongodbclient.connect(db_url, function(err,db){
+		if (err) throw err;
+		
+		var collection = db.collection("fcc-bookjump-books");
+		collection.insert(
+			data,
+			{},
+			function(err,doc) {
+				if (err) {
+					console.log(err);
+					res.end(err);
+				}
+				res.status(200).json(data);
+				res.end();
+		    }
+		);
+	});
+})
 
 app.listen(process.env.PORT || 5000, function(err){
 	if (err) {
---
+++ test/book.api.test.js
@@ -0,0 +1,114 @@
+var chai = require('chai');
+var expect = chai.expect;
+var chaiHttp = require('chai-http');
+var server = require('../app.js');
+
+chai.use(chaiHttp);
+chai.use(require('chai-json-schema'));
+
+describe('BOOK API', function(done){
+	var bookSchema, searchResultSchema, bookItem;
+
+	before(function(){
+		bookSchema = {
+			"title": "book schema",
+			"type": "object",
+			"required": ['title', 'authors', 'description', 'subtitle'],
+			"properties": {
+				"title": {
+					type: "string"
+				}
+			}
+		};
+
+		searchResultSchema = {
+			"title": "search result schema",
+			"type": "object",
+			"required": ['kind', 'totalItems', 'items'],
+			"properties": {
+				"title": {
+					type: "string"
+				},
+				"items": {
+					type: "array"
+				}
+			}
+		};
+
+		bookItem =  {
+			"title": "Abundance for What?",
+			"authors": [
+				"David Riesman"
+			],
+			"publisher": "Transaction Publishers",
+			"publishedDate": "1964",
+			"description": "This classic collection of essays by Riesman discusses the implications of affluence in America. Riesman maintains that the question that should be raised by wealth has shifted over time from how to obtain wealth to how to make use of it. Another key theme concerns issues relevant to higher education, such as academic freedom. This book examines the notion that America is not as open a society as it may appear to be; it shows how social science may be used to explain why this is so. In a brilliant, lengthy reevaluation Riesman both clarifies and revises that earlier assessment with unusual luster and candor.",
+			"industryIdentifiers": [
+				{
+					"type": "ISBN_10",
+					"identifier": "1412816335"
+				},
+				{
+					"type": "ISBN_13",
+					"identifier": "9781412816335"
+				}
+			],
+			"readingModes": {
+				"text": true,
+				"image": true
+			},
+			"pageCount": 626,
+			"printType": "BOOK",
+			"categories": [
+				"Social Science"
+			],
+			"maturityRating": "NOT_MATURE",
+			"allowAnonLogging": false,
+			"contentVersion": "0.0.3.0.preview.3",
+			"imageLinks": {
+				"smallThumbnail": "http://books.google.co.id/books/content?id=PJ3BAfhT72gC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+				"thumbnail": "http://books.google.co.id/books/content?id=PJ3BAfhT72gC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+			},
+			"language": "en",
+			"previewLink": "http://books.google.co.id/books?id=PJ3BAfhT72gC&printsec=frontcover&dq=abundance&hl=&cd=1&source=gbs_api",
+			"infoLink": "http://books.google.co.id/books?id=PJ3BAfhT72gC&dq=abundance&hl=&source=gbs_api",
+			"canonicalVolumeLink": "http://books.google.co.id/books/about/Abundance_for_What.html?hl=&id=PJ3BAfhT72gC"
+		};
+	});
+
+	xit('should return JSON containing all books on /book/findAll', function(done){
+		this.timeout(20000);//set timeout to 20 second
+		chai.request(server)
+			.get('/book/findAll')
+			.end(function(err, res){
+				expect(res).to.be.json;
+				expect(res).to.have.status(200);
+				expect(res.body[0]).to.be.jsonSchema(bookSchema);
+				done();
+			});
+	});
+
+	it('should return book search results with query : 4+hour', function(done){
+		this.timeout(20000);
+		chai.request(server)
+			.get('/book/search/4+hour+workweek')
+			.end(function(err, res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.json;
+				expect(res.body).to.be.jsonSchema(searchResultSchema);
+				done();
+			})
+	});
+
+	xit('should return status ok when adding book to collection', function(done){
+		this.timeout(20000);
+		chai.request(server)
+			.post('/book/add')
+			.field('data', JSON.stringify(bookItem))
+			.end(function(err,res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.json;
+				done();
+			});
+	});
+});
